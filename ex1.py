d26a4d4bbe733ed05186af54d5c24865041ecce0
kamunra <k.ismailzade11@gmail.com>
2021-12-18 03:27:07 +0400

    initial commit - distance function

---
+++ ex1.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from math import radians, cos, sin, acos, sqrt
+
+def distance(lat1, long1, lat2, long2):
+    lat1 = radians(lat1)
+    long1 = radians(long1)
+    lat2 = radians(lat2)
+    long2 = radians(long2)
+    
+    # Radius of earth in kilometers
+    r = 6371
+    
+    return (r*acos(sin(lat1)*sin(lat2)+cos(lat1)*cos(lat2)*cos(long2-long1)))
+
