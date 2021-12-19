751ad569334c7fc0a69219e631f92c4c3d144de9
Scorparri <nadirshikhli@gmail.com>
2021-12-19 20:05:16 +0400

    top10 degree connectivity

--- step_3_graph.py
+++ step_3_graph.py
@@ -20,8 +20,8 @@ def close_centrality(G):
     for key, value in close.items():
         close_values.append(value)
     return close_values
-def deg_centrality(G):
-    deg=nx.degree_centrality(G)
+def deg_connectivity(G):
+    deg=dict(nx.degree(G))
     deg_values=[]
     for key, value in deg.items():
         deg_values.append(value)
@@ -38,13 +38,13 @@ print('Average shortest path --> {}\n Plot density -->{}'.format(PlotAvPathL,Plo
 Nodes=[]
 between_values=bet_centrality(G)
 close_values=close_centrality(G)
-deg_values=deg_centrality(G)
+deg_values=deg_connectivity(G)
 
 with open("Node_characteristics.csv",'w') as f:
-    f.write("Node\t closeness centrality\t degree centrality\t betweenness centrality\n")  
+    f.write("Node\t closeness centrality\t degree connectivity\t betweenness centrality\n")  
     for i in range(0, len(close_values)):
         f.write("{0}\t{1}\t{2}\t{3}\n".format(Nodes[i],close_values[i],deg_values[i],between_values[i]))
 
-deg_cen=nx.degree_centrality(G)
-top10_deg_con = dict(sorted(deg_cen.items(), key=operator.itemgetter(1),reverse=True)[:10])
+deg_conn=dict(nx.degree(G))
+top10_deg_con = dict(sorted(deg_conn.items(), key=operator.itemgetter(1),reverse=True)[:10])
 print(top10_deg_con)
