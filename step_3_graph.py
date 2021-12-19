0d44ac87cfc252875d8dc6b71085990bf59b2ee9
kamunra <k.ismailzade11@gmail.com>
2021-12-19 22:00:12 +0400

    Comments on step_3 file

--- step_3_graph.py
+++ step_3_graph.py
@@ -31,43 +31,53 @@ data = pd.read_csv('routes.csv')
 SelReg = 'Asia'
 regionData = data.loc[(data['Source Region'].str.startswith(SelReg)) & data['Destination Region'].str.startswith(SelReg)]
 G = nx.from_pandas_edgelist(regionData,source = 'Source airport', target = 'Destination airport',edge_attr = True,create_using=nx.DiGraph)
+
+#Finding Network density average shortest path length
 PlotAvPathL = nx.average_shortest_path_length(G)
 PlotDensity = nx.density(G)
 
-print('Average shortest path --> {}\n Plot density -->{}'.format(PlotAvPathL,PlotDensity))
+# print('Average shortest path --> {}\n Plot density -->{}'.format(PlotAvPathL,PlotDensity))
+
 Nodes=[]
 between_values=bet_centrality(G)
 close_values=close_centrality(G)
 deg_values=deg_connectivity(G)
 
+# Storing the calculated node attribute values to a separate file
 with open("Node_characteristics.csv",'w') as f:
     f.write("Node\t closeness centrality\t degree connectivity\t betweenness centrality\n")  
     for i in range(0, len(close_values)):
         f.write("{0}\t{1}\t{2}\t{3}\n".format(Nodes[i],close_values[i],deg_values[i],between_values[i]))
 
+# Finding the top 10 nodes based on their degree connectivity values
 deg_conn=dict(nx.degree(G))
 top10_deg_con = dict(sorted(deg_conn.items(), key=operator.itemgetter(1),reverse=True)[:10])
 
 keys_deg = top10_deg_con.keys()
 values_deg = top10_deg_con.values()
 
-plt.bar(keys_deg, values_deg)
-plt.show()
+# Plotting them 
+# plt.bar(keys_deg, values_deg)
+# plt.show()
+
+# Finding the number of nodes with the least degree connectivity value - 2
 countOfDegTwo=0
 for key, value in deg_conn.items():
     if value==2:
         countOfDegTwo+=1
-print(countOfDegTwo)
+# print(countOfDegTwo)
 
+# Finding the greatest and least values of closeness centrality among nodes
 close_cent=nx.closeness_centrality(G)
 max_close_cent=max(close_cent.items(), key=operator.itemgetter(1)) 
 min_close_cent=min(close_cent.items(), key=operator.itemgetter(1))  
-print(max_close_cent)
-print(min_close_cent)
+# print(max_close_cent)
+# print(min_close_cent)
 
+# Finding and plotting the top 10 nodes by their betweenness centrality values
 between_cent=nx.betweenness_centrality(G)
 top10_bet_cent=dict(sorted(between_cent.items(), key=operator.itemgetter(1),reverse=True)[:10])
 keys_bet = top10_bet_cent.keys()
 values_bet = top10_bet_cent.values()
-plt.bar(keys_bet, values_bet)
-plt.show()
+# plt.bar(keys_bet, values_bet)
+# plt.show()
